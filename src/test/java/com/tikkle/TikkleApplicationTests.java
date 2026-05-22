c11e51232f42d5a3b3c8febe6fb8a3beb25dc821
김정윤 <canach3@gmail.com>
2026-05-22 19:09:18 +0900

    Spring Boot 초기 프로젝트 구조 세팅

---
+++ src/test/java/com/tikkle/TikkleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikkleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
