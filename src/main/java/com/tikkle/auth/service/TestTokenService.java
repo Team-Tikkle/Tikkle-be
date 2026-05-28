0761146cbf57020b0dadb9607502c8bb0a45b64c
Lee <ah0416@naver.com>
2026-05-28 18:00:04 +0900

    #18]fix: TestTokenService 유저 검증 추가 및 Remove-Item 권한 제거

--- src/main/java/com/tikkle/auth/service/TestTokenService.java
+++ src/main/java/com/tikkle/auth/service/TestTokenService.java
@@ -4,6 +4,9 @@ import com.tikkle.auth.dto.response.TokenResponse;
 import com.tikkle.auth.entity.RefreshToken;
 import com.tikkle.auth.jwt.JwtProvider;
 import com.tikkle.auth.repository.RefreshTokenRepository;
+import com.tikkle.user.entity.UserStatus;
+import com.tikkle.user.exception.UserNotFoundException;
+import com.tikkle.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -14,8 +17,11 @@ import org.springframework.stereotype.Service;
 public class TestTokenService {
     private final JwtProvider jwtProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
 
     public TokenResponse generateTestToken(String email) {
+        userRepository.findByEmailAndStatus(email, UserStatus.ACTIVE)
+                .orElseThrow(UserNotFoundException::new);
         final String accessToken = jwtProvider.createAccessToken(email);
         final String refreshToken = jwtProvider.createRefreshToken(email);
         refreshTokenRepository.save(new RefreshToken(
