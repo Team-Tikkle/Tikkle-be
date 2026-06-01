04639010d3d8bd1dc516270db3da45533feaba75
Lee <ah0416@naver.com>
2026-06-01 11:46:45 +0900

    [#21]refactor: Swagger 운영 서버 URL .xyz로 변경

--- src/main/java/com/tikkle/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/tikkle/global/config/swagger/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
                 .info(apiInfo())
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("로컬 서버"),
-                        new Server().url("https://api.tikkle.com").description("운영 서버")))
+                        new Server().url("https://api.tikkle.xyz").description("운영 서버")))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
