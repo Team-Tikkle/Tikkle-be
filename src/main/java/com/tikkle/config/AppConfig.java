4365ac85b649d36c473165fcbd00eba71119c3c0
JungYoon <canach3@gmail.com>
2026-06-16 10:53:03 +0900

    [#46]fix: 한국 기준 시간 적용(시간 불일치 문제 해결)

--- src/main/java/com/tikkle/config/AppConfig.java
+++ src/main/java/com/tikkle/config/AppConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 @Configuration
 public class AppConfig {
     @Bean
     public Clock clock() {
-        return Clock.systemDefaultZone();
+        return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 }
